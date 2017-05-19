f81f1faf928a8879c65a883c71f9415207fe9323
Viktor Gomeniuk <viktor.gomeniuk@gmail.com>
2017-05-19 23:46:14 +0300

    Load frame funcitonality

--- CyborgBattle/frame.cpp
+++ CyborgBattle/frame.cpp
@@ -1,6 +1,9 @@
+#include <sstream>
+#include <string>
 #include "frame.h"
 #include "drawing.h"
 #include "globals.h"
+#include "groupBuilder.h"
 
 void Frame::draw(SDL_Texture* spriteSheet, float x, float y) {
 	/* Where to draw the frame */
@@ -12,5 +15,44 @@ void Frame::draw(SDL_Texture* spriteSheet, float x, float y) {
 	};
 	renderTexture(spriteSheet, Globals::renderer, dest, &clip);
 
+}
+
+void Frame::loadFrame(std::ifstream& file, std::list<DataGroupType>& groupTypes) {
+	//Ok, we dragged these groupTypes all the way here, so we could construct groups on our frame to hold the data in
+	GroupBuilder::buildGroups(groupTypes, frameData);
+
+	// Helper buffers
+	std::string buffer;
+	std::stringstream ss;
+
+	// frame & clip
+	std::getline(file, buffer);
+	std::getline(file, buffer);
+	buffer = Globals::clipOffDataHeader(buffer);
+	ss << buffer;
+	ss >> clip.x >> clip.y >> clip.w >> clip.h;
+	ss.clear();
+
+	// offset/pivot
+	std::getline(file, buffer);
+	buffer = Globals::clipOffDataHeader(buffer);
+	ss << buffer;
+	ss >> offSet.x >> offSet.y;
+	ss.clear();
+
+	// duration
+	std::getline(file, buffer);
+	buffer = Globals::clipOffDataHeader(buffer);
+	ss << buffer;
+	ss >> duration;
+	ss.clear();
+
+	// index
+	std::getline(file, buffer);
+	buffer = Globals::clipOffDataHeader(buffer);
+	ss << buffer;
+	ss >> frameNumber;
+
+	GroupBuilder::loadGroups(file, frameData);
 }
 
--- CyborgBattle/frame.h
+++ CyborgBattle/frame.h
@@ -1,19 +1,26 @@
 #ifndef FRAME_H
 #define FRAME_H
 
+#include <iostream>
 #include <list>
 #include <SDL.h>
+#include "group.h"
+#include "dataGroupType.h"
 
 
 class Frame {
+public:
+	void draw(SDL_Texture* spriteSheet, float x, float y);
+	void loadFrame(std::ifstream& file, std::list<DataGroupType>& groupTypes);
+	int getFrameNumber() { return frameNumber; };
+
+private:
 	int frameNumber;  // Frame index
 	SDL_Rect clip;  // Region on spritesheet
 	float duration;  // How long frame runs
 	SDL_Point offSet;  // Pivot for frame allignment
-	std::list<void*> frameData;  // Groups of additional data
+	std::list<Group*> frameData;  // Groups of additional data
 
-	void draw(SDL_Texture* spriteSheet, float x, float y);
-	// TODO loadframe
 };
 
 #endif // !FRAME_H
