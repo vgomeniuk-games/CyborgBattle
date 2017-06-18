8445ca3f538b97735174a944437ca4e68d9d0a7a
Viktor Gomeniuk <viktor.gomeniuk@gmail.com>
2017-06-18 21:44:14 +0300

    Implement all the Hero function. New conception of HeroAnimations names access;

--- CyborgBattle/hero.cpp
+++ CyborgBattle/hero.cpp
@@ -1,29 +1,31 @@
+#include <vector>
+#include <string>
+
 #include "hero.h"
 #include "globals.h"
+#include "timeController.h"
 
-const std::string HERO_ANIM_UP = "up";
-const std::string HERO_ANIM_DOWN = "down";
-const std::string HERO_ANIM_LEFT = "left";
-const std::string HERO_ANIM_RIGHT = "right";
-const std::string HERO_ANIM_IDLE_UP = "idleUp";
-const std::string HERO_ANIM_IDLE_DOWN = "idleDown";
-const std::string HERO_ANIM_IDLE_LEFT = "idleLeft";
-const std::string HERO_ANIM_IDLE_RIGHT = "idleRight";
-const std::string HERO_ANIM_SLASH_UP = "slashUp";
-const std::string HERO_ANIM_SLASH_DOWN = "slashDown";
-const std::string HERO_ANIM_SLASH_LEFT = "slashLeft";
-const std::string HERO_ANIM_SLASH_RIGHT = "slashRight";
-const std::string HERO_ANIM_DASH_UP = "dashUp";
-const std::string HERO_ANIM_DASH_DOWN = "dashDown";
-const std::string HERO_ANIM_DASH_LEFT = "dashLeft";
-const std::string HERO_ANIM_DASH_RIGHT = "dashRight";
-const std::string HERO_ANIM_DASH_DIE = "die";
+struct HeroState {
+	static const int Idle;
+	static const int Move;
+	static const int Slash;
+	static const int Dash;
+	static const int Dead;
+};
+const int HeroState::Idle = 0;
+const int HeroState::Move = 1;
+const int HeroState::Slash = 2;
+const int HeroState::Dash = 3;
+const int HeroState::Dead = 4;
 
-const int AnimationState::Idle = 0;
-const int AnimationState::Move = 1;
-const int AnimationState::Slash = 2;
-const int AnimationState::Dash = 3;
-const int AnimationState::Dead = 4;
+std::vector<std::vector<std::string>> HeroAnimations = {
+	/* Direction:	Up,			Down			Left			Right */
+	/* Idle */	{ "idleUp",		"idleDown",		"idleLeft",		"idleRight" },
+	/* Move */	{ "up",			"down",			"left",			"right" },
+	/* Slash */	{ "slashUp",	"slashDown",	"slashLeft",	"slashRight" },
+	/* Dash */	{ "dashUp" ,	"dashDown",		"dashLeft",		"dashRight" },
+	/* Die */	{ "die",		"die",			"die",			"die"}
+};
 
 
 Hero::Hero(AnimationSet* animSet) {
@@ -31,48 +33,128 @@ Hero::Hero(AnimationSet* animSet) {
 	this->type = "hero";
 
 	// Setup default Hero values
-	this->x = Globals::WIDTH / 2;
-	this->y = Globals::HEIGHT / 2;
 	this->moveSpeed = 0;
 	this->moveSpeedMax = 80;
-	this->hp = 20;
 	this->hpMax = 20;
 	this->damage = 0;
 	this->collisionBoxWidth = 20;
 	this->collisionBoxHeight = 24;
 	this->collisionBoxYOffset = -20;
 	this->direction = Direction::Down;
-	changeAnimation(AnimationState::Idle, true);
+	revive();
+	updateCollisionBox();
 }
 
 void Hero::update() {
-
+	// Check if dead
+	if (hp < 1 && state != HeroState::Dead) {
+		changeAnimation(HeroState::Dead, true);
+		die();
+		return;  // TODO
+	}
+	// Update hero
+	updateCollisionBox();
+	updateMovememnt();
+	updateCollisions();
+	updateHitBox();
+	updateDamage();
+	updateAnimation();
+	updateInvincibleTimer();
 }
 
 void Hero::slash() {
-
+	if (hp > 0 && (state == HeroState::Move || state == HeroState::Idle)) {
+		moving = false;
+		frameTimer = 0;
+		changeAnimation(HeroState::Slash, true);
+		// TODO add sound
+	}
 }
 
 void Hero::dash() {
+	if (hp > 0 && (state == HeroState::Move || state == HeroState::Idle)) {
+		moving = false;
+		frameTimer = 0;
 
+		// push the player in the travel direction
+		slideAngle = angle;
+		slideAmount = 300;
+		invincibleTimer = 0.1;
+
+		changeAnimation(HeroState::Dash, true);
+		// TODO add sound
+	}
 }
 
 void Hero::die() {
-
+	moving = false;
+	changeAnimation(HeroState::Dead, true);
 }
 
 void Hero::revive() {
-
+	hp = hpMax;
+	changeAnimation(HeroState::Idle, true);
+	moving = false;
+	x = Globals::WIDTH / 2;
+	y = Globals::HEIGHT / 2;
+	slideAmount = 0;
 }
 
 void Hero::changeAnimation(/*AnimationStates*/int newState, bool reset) {
-
+	state = newState;
+	currentAnimation = animations->getAnimation(HeroAnimations[newState][direction]);
+	currentFrame = currentAnimation->getFrame( reset ? 0 : currentFrame->getFrameNumber() );
 }
 
 void Hero::updateAnimation() {
-
+	if (currentFrame == nullptr || currentAnimation == nullptr) {
+		return;
+	}
+	// Set correct state if stop moving
+	if (state == HeroState::Move && !moving) {
+		changeAnimation(HeroState::Idle, true);
+	}
+	// Change frame if current one's duration exeed
+	frameTimer += TimeController::controller.getDeltaTime();
+	if (frameTimer >= currentFrame->getDuration()) {
+		// Animation end
+		if (currentFrame->getFrameNumber() == currentAnimation->getEndFrameNumber()) {
+			// Unless hero is slashing || dashing - restart animation from start
+			switch (state)
+			{
+			case HeroState::Slash:
+			case HeroState::Dash:
+				changeAnimation(HeroState::Move, true);
+				break;
+			default:
+				currentFrame = currentAnimation->getFrame(0);
+			}
+		}
+		// Move to next frame
+		else {
+			currentFrame = currentAnimation->getNextFrame(currentFrame);
+		}
+		// Restart frame duration timer
+		frameTimer = 0;
+	}
 }
 
 void Hero::updateDamages() {
-
+	if (active && hp > 0 && invincibleTimer <= 0) {
+		for (auto entity : Entity::entities) {
+			if (entity->isActive() && entity->getType() == "enemy") {
+				LivingEntity* enemy = (LivingEntity*)entity;
+				if (enemy->getDamage() > 0 && Entity::checkCollision(collisionBox, enemy->getHitBox())) {
+					hp -= enemy->getDamage();
+				}
+				// If still alive
+				if (hp > 0) {
+					invincibleTimer = 0.3;
+					// TODO play sound
+				}
+				slideAngle = Entity::getAngle(entity, this);
+				slideAmount = 200;
+			}
+		}
+	}
 }
--- CyborgBattle/hero.h
+++ CyborgBattle/hero.h
@@ -1,13 +1,10 @@
 #ifndef HERO_H
 #define HERO_H
 
-#include <string>
 #include "entity.h"
 #include "livingEntity.h"
 #include "animationSet.h"
 
-
-struct AnimationState { static const int Idle; static const int Move; static const int Slash; static const int Dash; static const int Dead; };
 
 class Hero : public LivingEntity {
 public:
@@ -17,28 +14,13 @@ public:
 	void dash();
 	void die();
 	void revive();
-	void changeAnimation(/*AnimationStates*/int newState, bool reset);
+	void changeAnimation(/*HeroState*/int newState, bool reset);
 	void updateAnimation();
 	void updateDamages();
 
-private:
-	static const std::string HERO_ANIM_UP;
-	static const std::string HERO_ANIM_DOWN;
-	static const std::string HERO_ANIM_LEFT;
-	static const std::string HERO_ANIM_RIGHT;
-	static const std::string HERO_ANIM_IDLE_UP;
-	static const std::string HERO_ANIM_IDLE_DOWN;
-	static const std::string HERO_ANIM_IDLE_LEFT;
-	static const std::string HERO_ANIM_IDLE_RIGHT;
-	static const std::string HERO_ANIM_SLASH_UP;
-	static const std::string HERO_ANIM_SLASH_DOWN;
-	static const std::string HERO_ANIM_SLASH_LEFT;
-	static const std::string HERO_ANIM_SLASH_RIGHT;
-	static const std::string HERO_ANIM_DASH_UP;
-	static const std::string HERO_ANIM_DASH_DOWN;
-	static const std::string HERO_ANIM_DASH_LEFT;
-	static const std::string HERO_ANIM_DASH_RIGHT;
-	static const std::string HERO_ANIM_DASH_DIE;
+protected:
+	/*AnimationState*/int state;
+
 };
 
 #endif // !HERO_H
