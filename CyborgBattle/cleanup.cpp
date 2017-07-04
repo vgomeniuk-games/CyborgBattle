6cff05c878c935819c738b62e2ae8eb839753199
Viktor Gomeniuk <viktor.gomeniuk@gmail.com>
2017-07-04 07:48:30 +0300

    set *ptr's to nullptr after cleaning the resources.

--- CyborgBattle/cleanup.cpp
+++ CyborgBattle/cleanup.cpp
@@ -4,19 +4,23 @@
 void cleanup(SDL_Window *win){
 	if (!win) { return; }
 	SDL_DestroyWindow(win);
+	win = nullptr;
 }
 
 void cleanup(SDL_Renderer *ren){
 	if (!ren) { return; }
 	SDL_DestroyRenderer(ren);
+	ren = nullptr;
 }
 
 void cleanup(SDL_Texture *tex){
 	if (!tex) { return; }
 	SDL_DestroyTexture(tex);
+	tex = nullptr;
 }
 
 void cleanup(SDL_Surface *surf){
 	if (!surf) { return; }
 	SDL_FreeSurface(surf);
+	surf = nullptr;
 }
