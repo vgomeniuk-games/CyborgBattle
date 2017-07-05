5cb1ca5cf4889aa5a75edd384591dcfa0e1af879
Viktor Gomeniuk <viktor.gomeniuk@gmail.com>
2017-07-05 20:45:31 +0300

    Disable debug mode.

--- CyborgBattle/globals.cpp
+++ CyborgBattle/globals.cpp
@@ -4,7 +4,7 @@ const int Globals::WIDTH = 640;
 const int Globals::HEIGHT = 352;
 const int Globals::SCALE = 2;
 const float Globals::PI = 3.14159f;
-const bool Globals::DEBUG = true;
+const bool Globals::DEBUG = false;
 SDL_Renderer* Globals::renderer = nullptr;
 
 std::string Globals::clipOffDataHeader(std::string data) {
